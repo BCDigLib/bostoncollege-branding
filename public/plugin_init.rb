3efaa030921421140983d9acf089755363142e51
Akofa <ahiable@bc.edu>
2022-05-26 13:17:13 -0400

    Hide Subjects facet from filters list

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -4,4 +4,10 @@ Rails.application.config.after_initialize do
     # include accessrestrict in list of notes to fetch and render on search results page
     ABSTRACT = %w(abstract scopecontent accessrestrict)
   end
+
+
+  class SearchController < ApplicationController
+    # remove "Subjects" facet from "Additional filters" list
+    DEFAULT_SEARCH_FACET_TYPES = ['repository', 'primary_type', 'published_agents', 'langcode']
+  end
 end
