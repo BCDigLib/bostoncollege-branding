5909acd331cb4ea9c0e98ac3fa9a2bb67c4d18c1
Jesse Martinez <mjesse@gmail.com>
2022-06-01 12:37:01 -0400

    include PUI settings into plugin_init.rb #34

--- plugin_init.rb
+++ plugin_init.rb
@@ -1,3 +1,53 @@
+# These are the custom config overrides for this local instance of archivesspace
+AppConfig[:enable_public] = true
+AppConfig[:pui_hide][:repositories] = true
+AppConfig[:pui_hide][:accessions] = true
+AppConfig[:pui_hide][:subjects] = true
+AppConfig[:pui_hide][:container_inventory] = true
+AppConfig[:pui_page_actions_request] = false
+AppConfig[:plugins] << 'request_list' << 'harvard_aeon'
+AppConfig[:request_list] = {
+  :button_position => 0,
+  :record_types => ['archival_object', 'resource', 'top_container'],
+  :request_handlers => {
+    :harvard_aeon => {
+      :name => 'Harvard Aeon',
+      :profile => :harvard_aeon,
+      :url => 'https://burnsaccount.bc.edu/logon?Action=11&Form=31',
+      :list_opts => {
+        :return_link_label => 'Return to Search Burns Archives',
+        :form_target => 'RequestForm',
+        :aeon_link_url => 'https://burnsaccount.bc.edu/logon',
+        :request_types => {
+          'Visit' => {
+            'RequestType' => 'Loan',
+            'UserReview' => 'No',
+            'SkipOrderEstimate' => '',
+          },
+           'Copy' => {
+            'RequestType' => 'Copy',
+            'UserReview' => 'No',
+            'SkipOrderEstimate' => 'Yes',
+          }
+        },
+        :format_options => [
+                            'Reference PDF (1 week)',
+                            'Reference JPG (1 week)'
+                           ],
+        :delivery_options => [
+                              'Download'
+                             ]
+      }
+    }
+  },
+  :repositories => {
+    :default => {
+      :handler => :harvard_aeon,
+    },
+  }
+}
+
+
 ## OVERRIDE VARIOUS METHODS/ ADD NEW METHODS
 Rails.application.config.after_initialize do
   # most recent file version: v3.2.0
