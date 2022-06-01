b832373b92670b12b965ff33a124bb433ee5e573
Jesse Martinez <mjesse@gmail.com>
2022-06-01 12:35:11 -0400

    update documentation #34

--- plugin_init.rb
+++ plugin_init.rb
@@ -1,13 +1,16 @@
 ## OVERRIDE VARIOUS METHODS/ ADD NEW METHODS
 Rails.application.config.after_initialize do
+  # most recent file version: v3.2.0
+  # https://github.com/archivesspace/archivesspace/blob/v3.2.0/public/app/models/record.rb
   class Record
-    # include accessrestrict in list of notes to fetch and render on search results page
+    # include "accessrestrict" in list of notes to fetch and render on search results page
     ABSTRACT = %w(abstract scopecontent accessrestrict)
   end
 
-
+  # most recent file version: v3.2.0
+  # https://github.com/archivesspace/archivesspace/blob/v3.2.0/public/app/controllers/search_controller.rb
   class SearchController < ApplicationController
-    # remove "Subjects" facet from "Additional filters" list
+    # remove "subjects" and "repository" facets from "Additional filters" list
     DEFAULT_SEARCH_FACET_TYPES = ['primary_type', 'published_agents', 'langcode']
   end
 end
