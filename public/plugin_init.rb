a32de50f4fc9c33377d59d50c1b42d85d3853491
Akofa <ahiable@bc.edu>
2022-06-09 10:47:13 -0400

    Remove subject facets for other search views

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -5,9 +5,20 @@ Rails.application.config.after_initialize do
     ABSTRACT = %w(abstract scopecontent accessrestrict)
   end
 
-
   class SearchController < ApplicationController
     # remove "Subjects" facet from "Additional filters" list
     DEFAULT_SEARCH_FACET_TYPES = ['primary_type', 'published_agents', 'langcode']
+  end
+
+  class ResourcesController < ApplicationController
+    DEFAULT_RES_FACET_TYPES = %w{primary_type published_agents langcode}
+  end
+
+  class AgentsController < ApplicationController
+    DEFAULT_AG_FACET_TYPES = %w{primary_type}
+  end
+
+  class ObjectsController < ApplicationController
+    DEFAULT_OBJ_FACET_TYPES = %w(repository primary_type published_agents langcode)
   end
 end
