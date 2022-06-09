6c105f38314ad0833d7b3789ea717d3474ecb584
Akofa <ahiable@bc.edu>
2022-06-09 10:47:13 -0400

    Remove subject facets for other search views

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -66,4 +66,16 @@ Rails.application.config.after_initialize do
     # remove "subject" from list of record types to search
     DEFAULT_TYPES = %w{archival_object digital_object digital_object_component agent resource repository accession classification}
   end
+
+  class ResourcesController < ApplicationController
+    DEFAULT_RES_FACET_TYPES = %w{primary_type published_agents langcode}
+  end
+
+  class AgentsController < ApplicationController
+    DEFAULT_AG_FACET_TYPES = %w{primary_type}
+  end
+
+  class ObjectsController < ApplicationController
+    DEFAULT_OBJ_FACET_TYPES = %w(repository primary_type published_agents langcode)
+  end
 end
