496379c2496736a7beac8f2cae18a978a45214d3
Akofa <ahiable@bc.edu>
2022-08-15 14:44:54 -0400

    keeping track of changes

--- public/controllers/sitemap_controller.rb
+++ public/controllers/sitemap_controller.rb
@@ -1,11 +1,15 @@
 class SitemapController < ApplicationController
     
     def generate
-        @ASPACE_PUBLIC_URL = 'http://localhost:8081'
+        @aspace_public_url = 'http://localhost:8081'
    
-        @sitemap_agents = ["/agents/people/1450", "agents/people/1451"];
-        @sitemap_resources = ["/repositories/2/resources/390"];
-        @sitemap_digitalobjects = ["/repositories/2/digital_objects/2691"];
+        @sitemap_agents = ["/agents/people/1450", "agents/people/1451","/agents/people/1452","/agents/people/1453","/agents/people/1454"];
+        @sitemap_resources = ["/repositories/2/resources/390", "/repositories/2/resources/391", "/repositories/2/resources/392", "/repositories/2/resources/393","/repositories/2/resources/394"];
+        @sitemap_digitalobjects = ["/repositories/2/digital_objects/2690","/repositories/2/digital_objects/2691","/repositories/2/digital_objects/2692","/repositories/2/digital_objects/2693","/repositories/2/digital_objects/2694"];
+
+        @sitemap_agents.each do |agent|
+            File.write('sitemap.xml', "#{agent}\n", mode:'a')
+        end
 
         render "sitemap"
     end
