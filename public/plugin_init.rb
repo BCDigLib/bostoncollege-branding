a24f1d90b50363f019124893a4d61ab5c2d10386
akofa-a <99671168+akofa-a@users.noreply.github.com>
2024-01-31 13:37:37 -0500

    fix errors

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -168,5 +168,30 @@ Rails.application.config.after_initialize do
       final_counts['classification'] = final_counts['record_group']
       final_counts
     end
+
+     # extract the repository agent info
+    def process_repo_info(repo)
+      info = {}
+      info['top'] = {}
+      unless repo.nil?
+        %w(name uri url parent_institution_name image_url repo_code description).each do |item|
+          info['top'][item] = repo[item] unless repo[item].blank?
+        end
+        unless repo['agent_representation'].blank? || repo['agent_representation']['_resolved'].blank? || repo['agent_representation']['_resolved']['agent_contacts'].blank? || repo['agent_representation']['_resolved']['jsonmodel_type'] != 'agent_corporate_entity'
+          in_h = repo['agent_representation']['_resolved']['agent_contacts'][0]
+          %w{city region post_code country email }.each do |k|
+            info[k] = in_h[k] if in_h[k].present?
+          end
+          if in_h['address_1'].present?
+            info['address'] = []
+            [1, 2, 3].each do |i|
+              info['address'].push(in_h["address_#{i}"]) if in_h["address_#{i}"].present?
+            end
+          end
+          info['telephones'] = in_h['telephones'] if !in_h['telephones'].blank?
+        end
+      end
+      info
+    end
   end
 end
