0c00d2bbfac97724fa5de2f973dcec3b6187c8fa
akofa-a <99671168+akofa-a@users.noreply.github.com>
2026-04-23 07:50:27 -0400

    test removing a line of code

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -236,7 +236,7 @@ Rails.application.config.after_initialize do
         @sublist_action = "/repositories/2/"
         @result['count'] = resources
         #@page_title = strip_mixed_content(@result['name'])
-        @search = Search.new(params)
+        #@search = Search.new(params)
 
         render
 
